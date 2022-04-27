69e0a0f28ef1b443a50ca51f194647027097f07e
Iqbal Ajmeri <iqbal.ajmeri@operisoft.com>
2022-04-27 17:23:42 +0530

    version 6

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def home():
-	return "<h1>Demo Flask App V5</h1>"
+	return "<h1>Demo Flask App V6</h1>"
 
 if __name__=='__main__':
 	app.run(debug=True,host="0.0.0.0")
